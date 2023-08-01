3c4355f11f283cb505af0d5362efdea2f1e605bb
zaldyrs <128943313+zaldyrs@users.noreply.github.com>
2023-08-01 21:03:08 +0700

    Update updatedmrid.sh

--- updatedmrid.sh
+++ updatedmrid.sh
@@ -21,4 +21,9 @@ cat /tmp/DMRIdsKRAP.dat >> /usr/local/etc/DMRIds.dat
                                                                                                                                                      
 sed -i 's/Indonesia;ID;YB-YC-YD-YE-YF-YG-YH/Indonesia;ID;YB-YC-YD-YE-YF-YG-YH-JZ/g' /usr/local/etc/country.csv
 
+curl --fail -L -o/usr/local/etc/DMR_Hosts.txt -s https://hostfiles.w0chp.net/DMR_Hosts.txt
+
+curl --fail -L -o /tmp/DMR_HostsKRAP.txt -s https://raw.githubusercontent.com/zaldyrs/DMRsaya/main/DMR_Hosts.txt
+cat /tmp/DMR_HostsKRAP.txt >> /usr/local/etc/DMR_Hosts.txt
+
 
